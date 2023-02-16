aeb3825568ec9fce3872e4c04a822bf4cda27c96
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-16 15:02:04 -0800

    c code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - prints size of some data types
+ * Return: 0 if code exist else 1
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+
+}
