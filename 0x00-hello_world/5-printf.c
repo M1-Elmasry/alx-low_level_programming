9202548ebbcf3205ac12d73640d71f72cef1e897
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-16 14:40:34 -0800

    c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print somthing 
+ * Return: 0 if code exist else 1
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
