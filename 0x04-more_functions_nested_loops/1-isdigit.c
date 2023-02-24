e3cc18d58849c58a2d7a0f6c5b90c3ade7163035
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-24 05:35:16 -0800

    c code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks @c is number or not
+ * @c: number will checked
+ * Return: 1 if @c is digit 0 if not
+ */
+int _isdigit(int c)
+{
+	return ((c <= '9' && c >= '0'));
+}
