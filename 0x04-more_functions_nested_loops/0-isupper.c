62071611c3b1f1491afda9453177f5520fe86825
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-23 17:24:49 -0800

    c code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks characters is upper or not
- * @c: character
- * Return: 1 if the character isupper else 0
+ * _isupper - check if character @c is in uppercase
+ * @c: the character to check
+ * Return: 1 if @c is uppercase else 0
  */
 int _isupper(int c)
 {
-	if ((c >= 65) && (c <= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 'A' && c <= 'Z');
 }
