f3d3b3a56f4e219bbb87794b362f49c2149242a9
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-24 14:42:22 -0800

    c code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,11 @@ void print_most_numbers(void)
 
 	for (i = '0'; i < '9'; i++)
 	{
-		if((i != '4') && (i != '2'))
+		if((i == '4') || (i == '2'))
+		{
+			continue;
+		}
+		else
 		{
 			_putchar(i);
 		}
