39726231ffeff4b2c1b114baedd99a36e87bb361
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-26 16:10:35 -0800

    c code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints sum of all numbers that maltiples of 3 and 5
+ * below 1024
+ * Return: 0 if code exist else 1
+ */
+int main(void)
+{
+	int i, num;
+
+	num = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			num += i
+		}
+	}
+	return (0);
+}
