b50af4d670c88e471a63433a50a5f36f1984e4c0
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-27 16:59:58 -0800

    c code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 	i = 0;
 	while ((*(str + (i + 1)) != '\0') && (*(str + (i + 2)) != '\0'))
 	{
-		_putchar(*(str + i);
+		_putchar(*(str + i));
 		i += 2;
 	}
 	_putchar('\n');
