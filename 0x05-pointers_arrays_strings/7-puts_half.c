dbbdde1db25eea34e172c401f9081e6eaf7a14e2
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-27 17:26:31 -0800

    c code

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = 0; i < (len / 2); i++)
+		{
+			_putchar(*(str + (len / 2) + i));
+		}
+	}
+	else
+	{
+		for (i = 0; i <= ((len - 1) / 2); i++)
+		{
+			_putchar(*(str + ((len - 1) / 2) + i));
+		}
+	}
+	_putchar('\n');
+}
