d55538a63bb020de78846ff5660a44cf363ce1e4
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-27 17:39:16 -0800

    c code

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints every array element
+ * @a: pointer array
+ * @n: length of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", *a);
+
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", *(a + i));
+	}
+	printf('\n');
+}
