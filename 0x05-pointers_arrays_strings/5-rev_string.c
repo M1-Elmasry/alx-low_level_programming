97382faab87fcefd19eb31e471151e340ba5d7a9
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-02-27 16:22:17 -0800

    c code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,9 +15,11 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < (len / 2); i++)
 	{
+		temp = *(s + i);
 		*(s + i) = *(s + (len - i - 1));
+		*(s + (len - i - 1) = temp;
 	}
 }
 
