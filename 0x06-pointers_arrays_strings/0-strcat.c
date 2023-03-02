4fa257bfd3d80b3a33598338d87330e6c9df1365
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-02 07:39:43 -0800

    c code

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len;
+	
+	i = 0;
+	dest_len = 0;
+
+	while (*(dest + dest_len) != '\0')
+	{
+		dest_len++;
+	}
+
+	while (1)
+	{
+		if (*(src + i) != '\0')
+		{
+			*(dest + dest_len + i) = *(src + i);
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (dest);
+}
+
