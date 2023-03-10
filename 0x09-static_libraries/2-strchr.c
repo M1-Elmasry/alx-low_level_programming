891476291e1c44daa1b090cff4517fc16744c29a
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-10 12:31:29 -0800

    c code

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * len - get the length of string
+ * @s: string
+ * Return: len of @s
+ */
+int len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strchr - locate char @c in string @s
+ * @s: string
+ * @c: character
+ * Return: pointer points to @c in @s and if not in @s
+ * will return NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i < len(s); i++)
+	{
+		if (*(s + i) == c)
+		{
+			break;
+		}
+	}
+	return (*(s + i)  == c ? s + i : NULL);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return length of string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int ch;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (1)
+	{
+		ch = *(s + i);
+		if (ch <= 0)
+		{
+			return (len);
+		}
+		i++;
+		len++;
+	}
+}
