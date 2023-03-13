7337fccfc35ca5a1c82832330dc82e466cc076e4
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-13 15:42:42 -0700

    c code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-		int i;
-		int l;
-		char *s3;
+	int i;
+	int l;
+	char *s3;
 
-		(s1 == NULL) ? s1 = "" : s1;
-		(s2 == NULL) ? s2 = "" : s2;
+	(s1 == NULL) ? s1 = "" : s1;
+	(s2 == NULL) ? s2 = "" : s2;
 
-		s3 = malloc(strlen(s1) + strlen(s2));
+	s3 = malloc(strlen(s1) + strlen(s2) + 1 );
 
-		if (s3)
+	if (s3)
+	{
+	for (i = 0; i < (int) strlen(s1); i++)
 		{
-			for (i = 0; i < (int) strlen(s1); i++)
-			{
-				*(s3 + i) = *(s1 + i);
-			}
-
-			l = (int) strlen(s3);
-
-			for (i = 0; i < (int) strlen(s2); i++)
-			{
-				*(s3 + l + i) = *(s2 + i);
-			}
-			return (s3);
+			*(s3 + i) = *(s1 + i);
 		}
-		return (NULL);
+
+		l = (int) strlen(s3);
+
+		for (i = 0; i < (int) strlen(s2); i++)
+		{
+			*(s3 + l + i) = *(s2 + i);
+		}
+		return (s3);
+	}
+	return (NULL);
 }
