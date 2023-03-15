954ab4f79b9256117079463ac257f01f6626a13d
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-14 17:27:39 -0700

    c code

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,11 +11,18 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j;
-	char *out = calloc(1, ac * 4 + 25);
+	int i, j, l = 0, a = ac;
+	char *out = NULL;
 
 	if (ac > 0 && av != NULL)
 	{
+		while (a--)
+		{
+			l += (strlen(av[a]) + 1);
+
+		}
+		out = (char *) malloc(l + 1);
+
 		for (i = 0; i < ac; i++)
 		{
 			for (j = 0; j < (int) strlen(av[i]); j++)
