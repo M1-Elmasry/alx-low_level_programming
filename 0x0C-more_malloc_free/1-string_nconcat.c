8692ee15355ecd750c0da2d13c43050145ef742e
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-16 19:13:27 -0700

    c code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,33 +12,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int len2;
+	char *tab;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+	while (s1[len1])
+		len1++;
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+	while (s2[len2])
+		len2++;
 
-	len2 = strlen(s2) < n ? strlen(s2) : n;
+	tab = malloc(sizeof(char) * len1 + n + 1);
+	if (tab == NULL)
+		return (NULL);
 
-	p = malloc((strlen(s1) + len2 + 1) * sizeof(char));
+	for (i = 0; i < len1; i++)
+		tab[i] = s1[i];
 
-	if (p)
-	{
-		strcpy(p, s1);
+	for (j = 0; s2[j] && j < n; j++)
+		tab[i + j] = s2[j];
 
-		strncpy(p + strlen(s1), s2, len2);
+	tab[i + j] = '\0';
 
-		p[strlen(p)] = '\0';
-
-		return (p);
-
-	}
-	return (NULL);
+	return (tab);
 }
+
