aa0ce42a61fe261bd969faf62efba58b73630b16
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-20 14:17:29 -0700

    c code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,33 +1,11 @@
-#include <unistd.h>
-
-char _putchar(char c);
+#include <stdio.h>
 
 /**
- * _putchar - reimpelmetation to putchar function in standard library
- * @c: character
- * Return: write @c to stdout
- */
-char _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * main - print current file name
+ * main - prints current file name
  * Return: 0 if success else 1
  */
 int main(void)
 {
-	char *file_name;
-	int i;
-
-	file_name = __FILE__;
-
-	i = 0;
-	for (i = 0; file_name[i] != '\0'; i++)
-	{
-		_putchar(file_name[i]);
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
---
+++ 0x0D-preprocessor/20-main.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+char _putchar(char c);
+
+/**
+ * _putchar - reimpelmetation to putchar function in standard library
+ * @c: character
+ * Return: write @c to stdout
+ */
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - print current file name
+ * Return: 0 if success else 1
+ */
+int main(void)
+{
+	char *file_name;
+	int i;
+
+	file_name = __FILE__;
+
+	i = 0;
+	for (i = 0; file_name[i] != '\0'; i++)
+	{
+		_putchar(file_name[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
