9a95d74de1f20231a83ad5667f394bc4e6960314
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-23 17:08:35 -0700

    fix some errors with betty and NULL param sep

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,21 +10,22 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list args;
+        unsigned int i;
+        va_list args;
 
-	va_start(args, n);
+        va_start(args, n);
 
-	i = 0;
-	while (i < n)
-	{
-		printf("%d", va_arg(args, int));
-		if (separator != NULL && i != (n - 1))
-		{
-			printf("%s", separator);
-		}
-		i++;
-	}
-	va_end(args);
-	putchar('\n');
+        i = 0;
+        while (i < n)
+        {
+                printf("%d", va_arg(args, int));
+                if (separator != NULL && i != (n - 1))
+                {
+                        printf("%s", separator);
+                }
+                i++;
+        }
+        va_end(args);
+        putchar('\n');
 }
+
