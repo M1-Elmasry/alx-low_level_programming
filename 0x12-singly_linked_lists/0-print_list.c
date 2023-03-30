1ee14dc3d64dca6ae1d59580674938cc8da056d5
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-31 00:12:17 +0200

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_list - prints elements in each node in liked list 
+ * @list_t: head of the linked lists
+ *
+ * Return: number of nodes in linked list
+ */
+size_t print_list(const list_t *h)
+{
+  int n;
+  
+  n = 0;
+  while(h != NULL)
+  {
+    if (h->str)
+    {
+        printf("[%d] %s\n", h->len, h->str);
+        h = h->next;
+    }
+    else
+    {
+        printf("[0] (nil)\n");
+        h = h->next;
+    }
+    n++;
+  }
+  return (n);
+}
