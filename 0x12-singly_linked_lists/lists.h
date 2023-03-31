6c294d08f6018c4ec306686cc75346c343a195bf
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-30 19:11:42 -0700

    update prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef LIST_H
 #define LIST_H
 
@@ -20,6 +21,7 @@ typedef struct list_s
 /* functions prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 
 #endif
