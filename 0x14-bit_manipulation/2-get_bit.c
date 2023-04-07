4c80b0f44d3e1c0d4af97f78bb69f8c2d98d0aea
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-07 16:55:16 -0700

    fix: handle errors

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bit;
+	/* n_bits is number of bits in @n */
+	unsigned int bit, n_bits = 8 * sizeof(n);
 
-	if (index > 0)
+	if (index >= n_bits)
 		return (-1);
 
 	bit = (n >> index) & 1;
-	return (bit == 1 || bit == 0 ? bit : -1);
+	return (bit);
 }
