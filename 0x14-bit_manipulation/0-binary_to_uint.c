8ba40fbe835fb915b251bb401f48634a145acb85
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-04-06 19:29:51 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,52 @@
+#include <math.h>
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * power - simple impelementation to pow function in math.h
+ * @a: number
+ * @b: power
+ * Return: @a power @b
+ */
+int power(int a, int b)
+{
+	int i, sum;
+
+	sum = 1;
+	for (i = 0; i < b; i++)
+	{
+		sum *= a;
+	}
+	return (sum);
+}
+
+
+/**
+ * binary_to_uint - function to convert binary number to decimal number
+ * @b: pointer to string of 0s and 1s
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, sum, len = strlen(b);
+
+	sum = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			sum = 0;
+			break;
+		}
+
+		if (b[i] == '1')
+		{
+			sum += power(2, len - i - 1);
+		}
+	}
+	return (sum);
+}
