15f9457979ab6505ff3f1776c6b670005c6671ea
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-09 06:00:00 +0300

    update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <string.h>
+
+#define USAGE_ERROR 97
+#define READ_ERROR 98
+#define WRITE_ERROR 99
+#define CLOSE_ERROR 100
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *_itoa(int n);
 
 #endif
