0755ecb82dd3c9ca13d35f9a0c8f1316fe8bbfee
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-09 04:10:21 +0300

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - function that create file and and write in it
+ * @filename: filename
+ * @text_content: text that will written in file
+ * Return: 1 in success -1 in failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0;
+	int fp = open(filename, O_RDWR | O_CREAT, 0600);
+
+	if (!filename || !fp)
+		return (-1);
+
+	while (text_content[i])
+	{
+		if (write(fp, &text_content[i++], 1) == -1)
+			return (-1);
+	}
+
+	close(fp);
+	return (1);
+}
