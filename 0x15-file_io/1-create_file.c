3221cae1f6e2d7532adb396bc0aea1ad8047b3c2
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-09 04:12:41 +0300

    update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int i = 0;
-	int fp = open(filename, O_RDWR | O_CREAT, 0600);
+	int fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (!filename || !fp)
 		return (-1);
