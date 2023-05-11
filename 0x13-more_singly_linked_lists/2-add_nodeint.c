3d1817021b98206ceff001aeb3ee48da30212043
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-11 08:56:50 +0300

    add task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+  listint_t *node = malloc(sizeof(listint_t));
+
+  if (!node)
+    return (NULL);
+
+  node ->n = n;
+  node->next = *head;
+  *head = node;
+
+  return (node);
+}
