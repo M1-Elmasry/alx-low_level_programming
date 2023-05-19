792ee86fd58166577e7afb24018349bcfbf436c0
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-05-19 16:29:23 +0300

    update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
-char *_itoa(int n);
-
+/**
+ * main - cp content of file to another file
+ * @ac: number of argument
+ * @av: argv
+ * Return: 0 if success
+ */
 int main(int ac, char **av)
 {
 	size_t buff_size = 1024 * 3;
@@ -13,79 +17,34 @@ int main(int ac, char **av)
 
 	if (ac > 3 || ac < 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(USAGE_ERROR);
 	}
-
 	file_from = open(av[1], O_RDONLY);
 	if (read(file_from, buffer, buff_size) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't read from file ", 28);
-		write(STDERR_FILENO, av[1], strlen(av[1]));
-		write(STDERR_FILENO, "\n", 1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(READ_ERROR);
-
 	}
 	if (close(file_from) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd ", 22);
-		write(STDERR_FILENO, _itoa(file_from), strlen(_itoa(file_from)));
-		write(STDERR_FILENO, "\n", 1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(CLOSE_ERROR);
 	}
-
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (write(file_to, buffer, strlen(buffer)) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't write to ", 22);
-		write(STDERR_FILENO, av[2], strlen(av[2]));
-		write(STDERR_FILENO, "\n", 1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(WRITE_ERROR);
 	}
 	if (close(file_to) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd ", 22);
-		write(STDERR_FILENO, _itoa(file_to), strlen(_itoa(file_to)));
-		write(STDERR_FILENO, "\n", 1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(CLOSE_ERROR);
-	} close(file_to);
+	}
 
+	close(file_to);
 	free(buffer);
 
 	return (0);
-}
-
-/**
- * _itoa - simple impelementation to itoa function
- * this function convert the int number to string number
- * @n: integer number
- * Return: pointer to the number after converted to string
- */
-char *_itoa(int n)
-{
-	/**
-	 * Maximum length of a signed 32-bit integer
-	 * is 11 digits plus a null terminator
-	 */
-	char *s = malloc(12), c;
-	int i = 0, sign = n, j;
-
-	if (sign < 0)
-		n = -n;
-
-	do {
-		s[i++] = n % 10 + '0';
-	} while (n /= 10);
-
-	if (sign < 0)
-		s[i++] = '-';
-	s[i] = '\0';
-
-	for (j = 0; j < i / 2; j++)
-	{
-		c = s[j];
-		s[j] = s[i - j - 1];
-		s[i - j - 1] = c;
-	}
-	return (s);
 }
