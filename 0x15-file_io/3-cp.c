ed65f23693ae577668bba29c1b4fc18af4932196
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-07-11 02:59:28 +0300

    update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,21 +19,18 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(USAGE_ERROR);
 	}
-
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(READ_ERROR);
 	}
-
 	file_to = open(av[2], O_WRONLY | O_CREAT, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(WRITE_ERROR);
 	}
-
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(file_to, buffer, bytes_read);
@@ -43,24 +40,14 @@ int main(int ac, char **av)
 			exit(WRITE_ERROR);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(READ_ERROR);
 	}
-
-	if (close(file_from) == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(CLOSE_ERROR);
-	}
-
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(CLOSE_ERROR);
-	}
-
-	return (0);
+	} return (0);
 }
