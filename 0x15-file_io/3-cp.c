a0d495d2870ff35accdaefbda77d54fdd1cb1c5d
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-07-11 02:54:22 +0300

    update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
  * main - cp content of file to another file
  * @ac: number of argument
@@ -8,43 +10,57 @@
  */
 int main(int ac, char **av)
 {
-	size_t buff_size = 1024 * 3;
 	int file_from, file_to;
-	char *buffer = malloc(buff_size * sizeof(char));
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
-	if (!buffer)
-		return (0);
-
-	if (ac > 3 || ac < 3)
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(USAGE_ERROR);
 	}
+
 	file_from = open(av[1], O_RDONLY);
-	if (read(file_from, buffer, buff_size) == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(READ_ERROR);
 	}
+
+	file_to = open(av[2], O_WRONLY | O_CREAT, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(WRITE_ERROR);
+	}
+
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(WRITE_ERROR);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(READ_ERROR);
+	}
+
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(CLOSE_ERROR);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (write(file_to, buffer, strlen(buffer)) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(WRITE_ERROR);
-	}
+
 	if (close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(CLOSE_ERROR);
 	}
-
-	close(file_to);
-	free(buffer);
 
 	return (0);
 }
