61ebec0352a9b5b4a87469ac2e757e745229ae10
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-17 15:31:27 +0300

    update task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,27 +2,27 @@
 
 
 /**
- * add_dnodeint - add node at the beginning of a dlist 
- * @head: head of the dlist 
+ * add_dnodeint - add node at the beginning of a dlist
+ * @head: head of the dlist
  * @n: data of the new node
  * Return: address of the new node or NULL if failea
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *new_node = NULL, *temp = NULL;
+	dlistint_t *new_node = NULL, *temp = NULL;
 
-  if (!head)
-    return NULL;
+	if (!head)
+		return (NULL);
 
-  new_node = malloc(sizeof(dlistint_t));
-  if (!new_node)
-    return NULL;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-  temp = *head;
-  *head = new_node;
-  new_node->n = n;
-  new_node->next = temp;
-  new_node->prev = NULL;
+	temp = *head;
+	*head = new_node;
+	new_node->n = n;
+	new_node->next = temp;
+	new_node->prev = NULL;
 
-  return (new_node);
+	return (new_node);
 }
