a693d8f975d8e952f0b2a413e0811199e1ece1fb
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-09 02:35:32 +0300

    add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - initialize a hash table
+ * @size: size of hash table
+ *
+ * Return: pointer to allocated hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (!hash_table)
+		return (NULL);
+
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *));
+	if (!hash_table)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	hash_table->size = size;
+	return (hash_table);
+}
