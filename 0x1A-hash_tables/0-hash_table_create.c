90b6ddf8fb9a6e7c7c5cf7ba03947711dbe7b9de
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-09 02:47:43 +0300

    add task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
-	if (hash_table == NULL)
+	if (!hash_table)
 		return (NULL);
 
-	hash_table->size = size;
 
-	hash_table->array = malloc(sizeof(hash_node_t *));
-	if (hash_table == NULL)
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash_table)
 	{
 		free(hash_table);
 		return (NULL);
@@ -27,5 +26,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
 
+	hash_table->size = size;
 	return (hash_table);
 }
