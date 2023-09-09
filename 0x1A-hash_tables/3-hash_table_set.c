155eab53ac26be4741fe37537cf717abd2ad1201
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-09 05:06:35 +0300

    add task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -49,7 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index] == NULL)
-  		ht->array[index] = new_item;
+		ht->array[index] = new_item;
 	else
 		add_node_begin(&ht->array[index], new_item);
 
