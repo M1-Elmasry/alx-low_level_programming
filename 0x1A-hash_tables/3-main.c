e0e5b898f13e548243dd403d3fa1ae11a64c3d87
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-09 04:26:39 +0300

    add test file for task 3

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -10,17 +10,18 @@
  */
 int main(void)
 {
-	int index;
-	const unsigned char key[] = "hetairas";
+	int index, i;
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "betty", "cool");
 	hash_table_set(ht, "hetairas", "blabla");
 	hash_table_set(ht, "mentioner", "blabla2");
 	printf("setted\n");
-	index = key_index(key, 1024);
-	printf("index of %s = %d\n", key, index);
+	index = key_index((const unsigned char *)"hetairas", ht->size);
+	printf("index of hetairas = %d\n", index);
 	printf("%s\n", ht->array[index]->value);
+	i = hash_table_set(ht, "", "cool");
+	if (i == 0)
+		return (-1);
 	return (EXIT_SUCCESS);
 }
