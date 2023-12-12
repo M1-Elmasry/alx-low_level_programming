7f3427240341d24e12c46db790f8edfb1f7de853
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-12-12 23:12:16 +0200

    update the fuckin log msgs positions

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * linear_search_range - Searches for a value in a specified range of an array
+ * search - Searches for a value in a specified range of an array
  * using linear search.
  *
  * @array: Pointer to the array to be searched.
@@ -13,7 +13,7 @@
  *
  * Return: If the value is found, return the index; otherwise, return -1.
  */
-int linear_search_range(int *array, int start, int end, int value)
+int search(int *array, int start, int end, int value)
 {
 	int i;
 
@@ -36,12 +36,13 @@ int linear_search_range(int *array, int start, int end, int value)
  * @array: Pointer to the array to be searched.
  * @size: The number of elements in the array.
  * @value: The value to search for.
- *
  * Return: If the value is found, return the index; otherwise, return -1.
  */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t steps, i;
+	char *m1 = "Value found between indexes ";
+	char *m2 = "Value checked array";
 
 	if (!array)
 		return (-1);
@@ -52,14 +53,14 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[i] >= value)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n", i - steps, i);
-			return (linear_search_range(array, i - steps, i, value));
+			printf("%s[%lu] and [%lu]\n", m1, i > 0 ? i - steps : 0, i > 0 ? i : steps);
+			return (search(array, i > 0 ? i - steps : 0, i > 0 ? i : steps, value));
 		}
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("%s[%lu] = [%d]\n", m2, i, array[i]);
 		if (i + steps > size)
 		{
-			printf("Value found between indexes [%lu] and [%lu]\n", i, i + steps);
-			return (linear_search_range(array, i, (i + (size - i - 1)), value));
+			printf("%s[%lu] and [%lu]\n", m2, i, i + steps);
+			return (search(array, i, (i + (size - i - 1)), value));
 		}
 	}
 
